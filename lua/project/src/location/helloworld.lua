2a33c21a3cf4d879f6d884c1e40a4afa50464c1b
vibrantbyte <codingrun@163.com>
2019-04-28 15:29:17 +0800

    etc

--- lua/project/src/location/helloworld.lua
+++ lua/project/src/location/helloworld.lua
@@ -12,3 +12,26 @@ local function get_name()
 end
 
 ngx.say(get_name()..print_log.path);
+
+
+local incr_param = ARGV[1];
+local result = 1024 * 8;
+if (incr_param == '1') then
+    result = redis.call('INCR',KEYS[1]);
+elseif (incr_param == '-1') then
+    result = redis.call('DECR',KEYS[1]);
+end
+
+if(redis.call('TTL',KEYS[1]) < 0) then
+    redis.call('EXPIRE',KEYS[1],ARGV[1])
+end
+
+return result;
+
+
+
+
+
+
+
+local incr_param = ARGV[1];local result = 1024 * 8;if (incr_param == '1') then result = redis.call('INCR',KEYS[1]); else result = redis.call('DECR',KEYS[1]); end if( redis.call('TTL',KEYS[1]) < 0) then redis.call('EXPIRE',KEYS[1],ARGV[2]); end return result;
